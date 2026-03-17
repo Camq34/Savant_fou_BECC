a72f21425dd37b68ab3d593421eb7bcb56eff345
Eloise Nallet <eloise.nallet@epfedu.fr>
2026-03-17 16:30:04 +0100

    voir niv1

--- assets/laser.tsx
+++ assets/laser.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="laser" tilewidth="32" tileheight="32" tilecount="100" columns="7">
+ <properties>
+  <property name="collision" type="bool" value="false"/>
+ </properties>
  <image source="lasers.png" width="233" height="250"/>
 </tileset>
--- scenes/Niveau1.js
+++ scenes/Niveau1.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#2d2d2d', // Fond gris foncé pour éviter le blanc
+    backgroundColor: '#000000', // Fond gris foncé pour éviter le blanc
     scene: {
         preload: preload,
         create: create
@@ -14,8 +14,7 @@ const game = new Phaser.Game(config);
 function preload() {
     // 1. On charge les images depuis le dossier 'asset'
     // Assure-toi que les noms de fichiers .png sont exactement ceux-là
-    this.load.image('img_laser', 'assets/laser.png');
-    this.load.image('img_brique', 'assets/brique.png');
+    this.load.image('img_lasers', 'assets/lasers.png');
     this.load.image('img_items', 'assets/items.png');
 
     // 2. On charge la map JSON depuis le dossier 'asset'
@@ -28,16 +27,21 @@ function create() {
 
     // 3. Liaison entre le nom dans Tiled et l'image chargée
     // 'laser' et 'brique' doivent être les noms des onglets dans Tiled
-    const tilesetLaser = map.addTilesetImage('laser', 'img_laser');
-    const tilesetBrique = map.addTilesetImage('brique', 'img_brique');
+    const tilesetLasers = map.addTilesetImage('lasers', 'img_lasers');
     const tilesetItems = map.addTilesetImage('tiles_tiny_sample_2', 'img_items');
 
     // 4. Création du calque (Vérifie bien le nom exact à droite dans Tiled)
     // On met les tilesets dans un tableau [ ] au cas où tu utilises les deux
-    const layer = map.createLayer('Calque de Tuiles 1', [tilesetLaser, tilesetBrique], 0, 0);
+    const layer = map.createLayer('Calque de Tuiles 1', [tilesetLasers, tilesetItems], 0, 0);
 
     console.log(layer)
 
     // Centrer la caméra
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    // Zoom arrière pour voir si la map est cachée quelque part
+
+    // Force la caméra à aller au centre de la map
+    this.cameras.main.centerOn(map.widthInPixels / 2, map.heightInPixels / 2);
+
+    this.cameras.main.setZoom(0.415); // On dézoome un peu
 }
