2ea74ce1c16312d8390410947f3374037c465f51
Eloise Nallet <eloise.nallet@epfedu.fr>
2026-03-17 11:53:19 +0100

    enregistre map

--- assets/preview_1366.tsx
+++ assets/preview_1366.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="preview_1366" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="preview_1366.png" width="250" height="250"/>
+ <image source="brique.png" width="250" height="250"/>
 </tileset>
