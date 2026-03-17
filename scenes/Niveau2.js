3dc48b300f9285b284df15a3d22c4a352f808673
basile <basile.stortz@epfedu.fr>
2026-03-17 20:06:29 +0100

    nv 2 ameliorer

--- scenes/Niveau2.js
+++ scenes/Niveau2.js
@@ -1,63 +1,125 @@
-export default class niveau2 extends Phaser.Scene {
-  // constructeur de la classe
+class niveau2 extends Phaser.Scene {
   constructor() {
     super({
-      key: "niveau2" //  ici on précise le nom de la classe en tant qu'identifiant
+      key: "niveau2"
     });
   }
+
   preload() {
-    this.preload.tilemapTiledJSON('map_niveau2', 'assets/Map/map_niveau2:5.tmj');
-    this.preload.image('img_materiaux', 'assets/donjonasset.png');
-    this.preload.image('img_materiaux', 'assets/terrain_d2_70.jpg');
-    this.preload.image('img_coffrebleu', 'assets/coffreBLEU.png');
-    this.preload.image('img_coffrerouge', 'assets/coffreROUGE.png');
-    this.preload.image('img_coffrevert', 'assets/coffreVERT.png');
-    this.preload.image('img_coffrejaune', 'assets/coffreJAUNE.png');
-    this.preload.image('img_portesortie', 'assets/portesortiewallah.png');
-    this.load.spritesheet("img_perso", "src/assets/savant2.png", {
-      frameWidth: 100,
-      frameHeight: 450
+    this.load.tilemapTiledJSON('map_niveau2', 'assets/Map/mapniveau2_5.tmj');
+    this.load.image('img_terrain', 'assets/terrain_d2_70.jpg');
+    this.load.image('img_donjonasset', 'assets/donjonasset.png');
+    this.load.image('img_brique', 'assets/brique.png');
+    this.load.image('img_coffre_jaune', 'assets/coffre_jaune.png');
+    this.load.image('img_coffre_rouge', 'assets/coffre_rouge.png');
+    this.load.image('img_coffre_bleu', 'assets/coffre_bleu.png');
+    this.load.image('img_coffre_vert', 'assets/coffre_vert.png');
+    this.load.image('img_portesortie', 'assets/portesortiewallah.png');
+    this.load.spritesheet("savant2", "assets/savant2.png", {
+      frameWidth: 50,
+      frameHeight: 50
     });
   }
 
   create() {
-    
-    // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 2", {
-      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-      fontSize: "22pt"
-    });
+    const map = this.make.tilemap({ key: "map_niveau2" });
 
-    this.porte_retour = this.physics.add.staticSprite(100, 550, "img_portesortiewallah");
+    const tilesetTerrain = map.addTilesetImage("terrain_d2_70", "img_terrain");
+    const tilesetDonjon = map.addTilesetImage("donjonasset", "img_donjonasset");
+    const tilesetBrique = map.addTilesetImage("brique", "img_brique");
+    const tilesetCoffreJaune = map.addTilesetImage("coffre_jaune", "img_coffre_jaune");
+    const tilesetCoffreRouge = map.addTilesetImage("coffre_rouge", "img_coffre_rouge");
+    const tilesetCoffreBleu = map.addTilesetImage("coffre_bleu", "img_coffre_bleu");
+    const tilesetCoffreVert = map.addTilesetImage("coffre_vert", "img_coffre_vert");
+    const tilesetPorte = map.addTilesetImage("portesortiewallah", "img_portesortie");
 
-    this.player = this.physics.add.sprite(100, 450, "img_perso");
-    this.player.refreshBody();
+    const allTilesets = [tilesetTerrain, tilesetDonjon, tilesetBrique, tilesetCoffreJaune, tilesetCoffreRouge, tilesetCoffreBleu, tilesetCoffreVert, tilesetPorte];
+
+    map.createLayer("Tile Layer 1", allTilesets);
+    const layerDecor = map.createLayer("décor", allTilesets);
+
+    // Activer les collisions sur les tiles avec la propriété "collision"
+    layerDecor.setCollisionByProperty({ collision: true });
+
+    // Personnage en bas à gauche, sur le sol
+    this.player = this.physics.add.sprite(100, 1100, "savant2", 5);
+    this.player.setScale(2);
+    this.player.body.setSize(50, 50);
+    this.player.body.setOffset(0, 0);
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
+    this.player.setDepth(10);
+
+    // Collision entre le joueur et le layer décor
+    this.physics.add.collider(this.player, layerDecor);
+
+    // Animations
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("savant2", { start: 0, end: 4 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "idle",
+      frames: [{ key: "savant2", frame: 5 }],
+      frameRate: 10
+    });
+
+    this.anims.create({
+      key: "jump",
+      frames: this.anims.generateFrameNumbers("savant2", { start: 6, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // Clavier
     this.clavier = this.input.keyboard.createCursorKeys();
-    this.physics.add.collider(this.player, this.groupe_plateformes);
   }
 
   update() {
-    if (this.clavier.left.isDown) {
-      this.player.setVelocityX(-160);
-      this.player.anims.play("anim_tourne_gauche", true);
-    } else if (this.clavier.right.isDown) {
+    if (this.clavier.right.isDown) {
       this.player.setVelocityX(160);
-      this.player.anims.play("anim_tourne_droite", true);
+      this.player.setFlipX(false);
+      this.player.anims.play("right", true);
+    } else if (this.clavier.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.setFlipX(true);
+      this.player.anims.play("right", true);
     } else {
       this.player.setVelocityX(0);
-      this.player.anims.play("anim_face");
-    }
-    if (this.clavier.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
+      this.player.anims.play("idle");
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-        console.log("niveau 3 : retour vers selection");
-        this.scene.switch("selection");
-      }
+    if (this.clavier.up.isDown && this.player.body.blocked.down) {
+      this.player.setVelocityY(-330);
+      this.player.anims.play("jump", true);
     }
   }
 }
+
+// Configuration du jeu
+var config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1280,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 300
+      },
+      debug: true
+    }
+  },
+  scene: [niveau2]
+};
+
+// Lancement du jeu
+var game = new Phaser.Game(config);
+game.scene.start("niveau2");
