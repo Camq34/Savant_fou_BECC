3ee6950f82ac306ebe6cd05a39cc0f016e29169d
Eloise Nallet <eloise.nallet@epfedu.fr>
2026-03-18 12:22:54 +0100

    porte 2 et 6

--- scenes/Accueil.js
+++ scenes/Accueil.js
@@ -59,11 +59,11 @@ export default class Accueil extends Phaser.Scene {
     /* CREATION DES PORTES */
     /*************************************/
     this.porte1 = this.physics.add.staticSprite(600, 414, "img_porte_orange");
-    this.porte2 = this.physics.add.staticSprite(50, 264, "img_porte_orange");
+    this.porte2 = this.physics.add.staticSprite(512, 613, "img_porte_orange");
     this.porte3 = this.physics.add.staticSprite(700, 234, "img_porte_orange");
     this.porte4 = this.physics.add.staticSprite(650, 234, "img_porte_orange");
     this.porte5 = this.physics.add.staticSprite(800, 234, "img_porte_orange");
-    this.porte6 = this.physics.add.staticSprite(900, 234, "img_porte_orange");
+    this.porte6 = this.physics.add.staticSprite(895, 740, "img_porte_orange");
     this.porte7 = this.physics.add.staticSprite(1000, 234, "img_porte_orange");
 
     /*************************************/
