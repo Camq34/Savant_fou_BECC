31da3afd46b830e85dd1108cfb87e93b81521aa5
Eloise Nallet <eloise.nallet@epfedu.fr>
2026-03-18 12:45:30 +0100

    map 4

--- assets/preview_122.tsx
+++ assets/preview_122.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="preview_122" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="laser.png" width="233" height="250"/>
+ <image source="lasers.png" width="224" height="224"/>
  <tile id="0">
   <properties>
    <property name="collision" type="bool" value="true"/>
