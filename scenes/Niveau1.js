753efc429884295f6db194851a38068cb0ed6b1e
conrado <conrado.grousset@epfedu.fr>
2026-03-19 14:12:01 +0100

    corrige anime du perso niveau 1

--- scenes/Niveau1.js
+++ scenes/Niveau1.js
@@ -9,8 +9,9 @@ export default class Niveau1 extends Phaser.Scene {
         this.load.tilemapTiledJSON('ma_map', 'assets/Map/map_niveau1.tmj');
 
         this.load.spritesheet("img_perso", "assets/savant2.png", {
-            frameWidth: 41,
-            frameHeight: 50
+            frameWidth: 40,
+            frameHeight: 50,
+            spacing: 1
         });
 
         this.load.spritesheet("img_porte", "assets/porteORANGE999.png", {
@@ -43,11 +44,13 @@ export default class Niveau1 extends Phaser.Scene {
         this.playerStartX = playerStartX;
         this.playerStartY = playerStartY;
         player = this.physics.add.sprite(playerStartX, playerStartY, 'img_perso');
-        player.setBounce(0.2);
+        player.setBounce(0);
         player.setCollideWorldBounds(true);
         player.setDepth(10); // dessiné au-dessus des portes
         player.setScale(1.6);
         player.setVisible(true);
+        player.body.setSize(20, 44);
+        player.body.setOffset(10, 6);
 
         this.isDying = false;
         const mourir = () => {
@@ -79,21 +82,22 @@ export default class Niveau1 extends Phaser.Scene {
         this.cameras.main.setZoom(mapZoom);
 
         this.anims.create({
-            key: "anim_tourne_gauche",
+            key: "savant2_idle",
+            frames: [{ key: "img_perso", frame: 5 }],
+            frameRate: 1,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "savant2_walk",
             frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 4 }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
-            key: "anim_tourne_droite",
+            key: "savant2_jump",
             frames: this.anims.generateFrameNumbers("img_perso", { start: 6, end: 10 }),
             frameRate: 10,
             repeat: -1
-        });
-        this.anims.create({
-            key: "anim_face",
-            frames: [{ key: "img_perso", frame: 5 }],
-            frameRate: 20
         });
 
         clavier = this.input.keyboard.createCursorKeys();
@@ -128,6 +132,8 @@ export default class Niveau1 extends Phaser.Scene {
             }
         });
 
+        player.play("savant2_idle");
+
         this.anims.create({
             key: "anim_ouvreporte",
             frames: this.anims.generateFrameNumbers("img_porte", { start: 0, end: 2 }),
@@ -147,19 +153,28 @@ export default class Niveau1 extends Phaser.Scene {
     update() {
         if (gameOver) return;
 
+        const isOnGround = player.body.blocked.down || player.body.touching.down;
+
         if (clavier.left.isDown) {
             player.setVelocityX(-160);
-            player.anims.play('anim_tourne_gauche', true);
+            player.setFlipX(true);
         } else if (clavier.right.isDown) {
             player.setVelocityX(160);
-            player.anims.play('anim_tourne_droite', true);
+            player.setFlipX(false);
         } else {
             player.setVelocityX(0);
-            player.anims.play('anim_face');
         }
 
-        if ((clavier.up.isDown || clavier.space.isDown) && player.body.blocked.down) {
+        if ((clavier.up.isDown || clavier.space.isDown) && isOnGround) {
             player.setVelocityY(-400);
+        }
+
+        if (!isOnGround) {
+            player.anims.play("savant2_jump", true);
+        } else if (player.body.velocity.x !== 0) {
+            player.anims.play("savant2_walk", true);
+        } else {
+            player.anims.play("savant2_idle", true);
         }
 
         if (Phaser.Input.Keyboard.JustDown(this.keyE)) {
